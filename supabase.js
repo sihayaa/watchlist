fd3b62b4438a823d5c30e0e7a43d935a33be42ab
sihayaa <gokumeng48@gmail.com>
2026-07-27 08:45:17 +0800

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://dhquxkzxskxaxmkvzklb.supabase.co";
+const SUPABASE_KEY = "sb_publishable_ksdXUj4t168FxDJXRaVJ1g_a_ND00jJ";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
